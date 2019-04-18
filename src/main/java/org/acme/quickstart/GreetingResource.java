4481cf926753bd56db31c0ffb1d045c710433979
Shariq Hirani <shariqhirani2010@gmail.com>
2019-04-18 17:18:23 -0500

    completed get-started

--- src/main/java/org/acme/quickstart/GreetingResource.java
+++ src/main/java/org/acme/quickstart/GreetingResource.java
@@ -1,16 +1,38 @@
 package org.acme.quickstart;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 @Path("/hello")
 public class GreetingResource {
+
+    @Inject
+    GreetingService service;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "hello";
     }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("greeting/{name}")
+    public String greeting(@PathParam("name") String name) {
+        return service.greeting(name);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("async")
+    public CompletionStage<String> asynchello() {
+        return CompletableFuture.supplyAsync(() -> "hello");
+    }
+
 }
